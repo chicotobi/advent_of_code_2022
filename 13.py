af73c3781f222fc21ce2fd1cdad87d44cd44b516
Tobias Hofmann (rm2mw061244) <tobias.hofmann.th1@roche.com>
2022-12-13 14:45:23 +0100

    part 2

--- 13.py
+++ 13.py
@@ -1,45 +1,44 @@
 d = open("13.txt", "r").read().splitlines()
 
 def compare(x,y):
-  #print(x,y)
   if type(x) == int and type(y) == int:
-    if x<y:
-      return True
-    if x>y:
-      return False
-    return None
+    if x < y:
+      return -1
+    elif x > y:
+      return 1
+    else:
+      return 0
   if type(y) == int:
     y = [y]
   if type(x) == int:
     x = [x]
   for i in range(min(len(x),len(y))):
     v = compare(x[i],y[i])
-    if v is not None:
+    if v != 0:
       return v
-  if len(x) > len(y):
-    return False
-  elif len(y) > len(x):
-    return True
+  if len(x) < len(y):
+    return -1
+  elif len(x) > len(y):
+    return 1
   else:
-    return None
+    return 0
 
 s = 0
 l = len(d)//3+1
 for i in range(l):
-  print()
-  print("new")
-  print()
   v1 = eval(d[3*i])
   v2 = eval(d[3*i+1])
-  print(v1)
-  print()
-  print(v2)
   v = compare(v1,v2)
-  if v is None:
-    print("big mistake")
-  elif v:
-    print("right order")
+  if v == -1:
     s += i+1
-  else:
-    print("not right order")
-    s += 0
+print(s)
+
+d2 = [eval(i) for i in d if i!=""] + [[[2]],[[6]]]
+
+import functools
+
+list.sort(d2, key=functools.cmp_to_key(compare))
+
+idx1 = [i for (i,v) in enumerate(d2) if v==[[2]]][0] + 1
+idx2 = [i for (i,v) in enumerate(d2) if v==[[6]]][0] + 1
+print(idx1*idx2)
