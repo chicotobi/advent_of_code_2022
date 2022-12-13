9f44e415cb1824e1cb0b56f9e2629bc5c0655122
Tobias Hofmann (rm2mw061244) <tobias.hofmann.th1@roche.com>
2022-12-13 14:33:16 +0100

    bug in algo

--- 13.py
+++ 13.py
@@ -8,31 +8,38 @@ def compare(x,y):
     if x>y:
       return False
     return None
-  if type(x) == list and type(y) == int:
-    return compare(x,[y])
-  if type(x) == int and type(y) == list:
-    return compare([x],y)
-  for i in range(len(x)):
-    if len(y) < i + 1:
-      return False
+  if type(y) == int:
+    y = [y]
+  if type(x) == int:
+    x = [x]
+  for i in range(min(len(x),len(y))):
     v = compare(x[i],y[i])
     if v is not None:
       return v
-  return True
+  if len(x) > len(y):
+    return False
+  elif len(y) > len(x):
+    return True
+  else:
+    return None
 
 s = 0
 l = len(d)//3+1
 for i in range(l):
-  #print()
-  #print()
+  print()
+  print("new")
+  print()
   v1 = eval(d[3*i])
   v2 = eval(d[3*i+1])
+  print(v1)
+  print()
+  print(v2)
   v = compare(v1,v2)
   if v is None:
     print("big mistake")
   elif v:
-    #print("right order")
+    print("right order")
     s += i+1
   else:
-    #print("not right order")
+    print("not right order")
     s += 0
